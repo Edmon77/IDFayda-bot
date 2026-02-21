3672880b5d25872227059b5c1c425a5f09b8e19a
Edmon77 <edmon7767@gmail.com>
2026-02-21 09:43:31 -0800

    Fix MongoDB connection: remove deprecated bufferMaxEntries option

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ const options = {
   minPoolSize: 5, // Maintain at least 5 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  bufferMaxEntries: 0, // Disable mongoose buffering
-  bufferCommands: false, // Disable mongoose buffering
+  // bufferMaxEntries and bufferCommands are deprecated in Mongoose 8+
+  // Mongoose handles buffering automatically now
 };
 
 let isConnected = false;
