26c810701e03f2dadbf2a957e5bcb1d1d449466a
Edmon <104652291+Edmon77@users.noreply.github.com>
2026-02-21 16:23:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const { Telegraf, session } = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+  handlerTimeout: 180000 // 180 seconds (3 minutes)
+});
 bot.use(session());
 
 module.exports = bot;
